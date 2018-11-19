297a74128c6d4595d2195e16ce430b2332a48596
Matthieu Chavigny <matttieu@gmail.com>
2018-11-19 18:46:10 +0100

    use const

--- js/NiceForm.js
+++ js/NiceForm.js
@@ -6,8 +6,8 @@ class NiceForm {
     
     initInputs() {
         $('.input').each((index, input)=>{
-            let $input = $(input).find('input');
-            let label = $input.attr('placeholder');
+            const $input = $(input).find('input');
+            const label = $input.attr('placeholder');
             $(input).attr('data-label-content', label);
             $input.on('focus', this.inputFocus.bind(this));
             $input.on('blur', this.inputBlur.bind(this));
@@ -16,17 +16,17 @@ class NiceForm {
     } 
 
     inputFocus(event) {
-        let $input = $(event.currentTarget);
+        const $input = $(event.currentTarget);
         $input.closest('.input').addClass('has-focus');
     }
     
     inputBlur(event) {
-        let $input = $(event.currentTarget);
+        const $input = $(event.currentTarget);
         $input.closest('.input').removeClass('has-focus');
     }
 
     inputChange(event) {
-        let $input = $(event.currentTarget);
+        const $input = $(event.currentTarget);
         if($input.val() ==='') {
             $input.closest('.input').removeClass('has-content');
         }
